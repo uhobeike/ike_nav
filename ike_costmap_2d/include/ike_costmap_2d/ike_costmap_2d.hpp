e691157fcab95526455d40942668f2cb62db07b6
Tatsuhiro Ikebe <beike315@icloud.com>
2023-08-12 23:21:16 +0900

    Feature ike_costmap_2d (#12)
    * Fix package.xml depend tag
    
    * Fix include guard
    
    * Add launch ike_costmap_2d composable_node
    
    * Feature static_layer inflation_layer publish
    
    * Add ike_costmap_2d
    
    * Fix inflation layer color scheme
    
    * Fix display name
    
    * Feature GetCostMap2D.srv
    
    * Feature get_costmap_2d service server
    
    * Fix service name & Add member variable map_
    
    * Change service used get_map to get_costmap_2d
    
    * Feature ike_costmap_2d

---
+++ ike_costmap_2d/include/ike_costmap_2d/ike_costmap_2d.hpp
@@ -0,0 +1,49 @@
+// SPDX-FileCopyrightText: 2023 Tatsuhiro Ikebe <beike315@icloud.com>
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef IKE_COSTMAP_2D__IKE_COSTMAP_2D_HPP_
+#define IKE_COSTMAP_2D__IKE_COSTMAP_2D_HPP_
+
+#include <rclcpp/rclcpp.hpp>
+
+#include "ike_nav_msgs/srv/get_cost_map2_d.hpp"
+#include "ike_nav_msgs/srv/get_map.hpp"
+#include <nav_msgs/msg/occupancy_grid.hpp>
+
+namespace ike_nav
+{
+
+class IkeCostMap2D : public rclcpp::Node
+{
+public:
+  explicit IkeCostMap2D(const rclcpp::NodeOptions & options);
+
+protected:
+  void initPublisher();
+  void initService();
+
+  std::map<std::string, nav_msgs::msg::OccupancyGrid> createCostMap2DLayers(
+    const nav_msgs::msg::OccupancyGrid & map);
+  nav_msgs::msg::OccupancyGrid createStaticLayer(const nav_msgs::msg::OccupancyGrid & map);
+  nav_msgs::msg::OccupancyGrid createInflationLayer(const nav_msgs::msg::OccupancyGrid & map);
+
+  nav_msgs::msg::OccupancyGrid getMap();
+
+  void publishCostMap2DLayers(
+    std::map<std::string, nav_msgs::msg::OccupancyGrid> & costmap_2d_layers);
+
+private:
+  rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr static_layer_pub_;
+  rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr inflation_layer_pub_;
+  // rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr obstacle_layer_pub_;
+  rclcpp::Service<ike_nav_msgs::srv::GetCostMap2D>::SharedPtr get_costmap_2d_srv_;
+  rclcpp::Service<ike_nav_msgs::srv::GetMap>::SharedPtr get_map_srv_;
+
+  std::map<std::string, nav_msgs::msg::OccupancyGrid> costmap_2d_layers_;
+
+  nav_msgs::msg::OccupancyGrid map_;
+};
+
+}  // namespace ike_nav
+
+#endif  // IKE_COSTMAP_2D__IKE_COSTMAP_2D_HPP_
