3ca72825fca6df214ae6b74da8f4490319de37b8
Tatsuhiro Ikebe <beike315@icloud.com>
2023-08-20 13:10:06 +0900

    Add thin out path

--- ike_controller/include/ike_controller/ike_controller.hpp
+++ ike_controller/include/ike_controller/ike_controller.hpp
@@ -27,7 +27,7 @@ protected:
   void initService();
 
   geometry_msgs::msg::TwistStamped ModelPredictiveControl(
-    const geometry_msgs::msg::PoseStamped & robot_pose, const nav_msgs::msg::Path & path);
+    const geometry_msgs::msg::PoseStamped & robot_pose, nav_msgs::msg::Path & path);
   void setMpcParameters();
   std::pair<std::vector<double>, std::vector<double>> convertPathXY(
     const nav_msgs::msg::Path & path);
