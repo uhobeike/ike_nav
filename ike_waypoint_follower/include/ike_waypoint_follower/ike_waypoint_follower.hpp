6b9af970a54864b8fb31451af2f462c231a146ef
uhobeike <beike315@icloud.com>
2023-08-24 19:39:26 +0900

    Feature readWaypointYaml

--- ike_waypoint_follower/include/ike_waypoint_follower/ike_waypoint_follower.hpp
+++ ike_waypoint_follower/include/ike_waypoint_follower/ike_waypoint_follower.hpp
@@ -4,7 +4,11 @@
 #ifndef IKE_WAYPOINT_FOLLOWER__IKE_WAYPOINT_FOLLOWER_HPP_
 #define IKE_WAYPOINT_FOLLOWER__IKE_WAYPOINT_FOLLOWER_HPP_
 
+#include "ike_waypoint_follower_parameter/ike_waypoint_follower_parameter.hpp"
+
 #include <rclcpp/rclcpp.hpp>
+
+#include "ike_nav_msgs/msg/waypoints.hpp"
 
 namespace ike_nav
 {
@@ -14,7 +18,16 @@ public:
   explicit IkeWaypointFollower(const rclcpp::NodeOptions & options);
 
 protected:
+  void getParam();
+
+  void readWaypointYaml();
+
 private:
+  std::shared_ptr<ike_waypoint_follower::ParamListener> param_listener_;
+  ike_waypoint_follower::Params params_;
+
+  std::string waypoint_yaml_path_;
+  ike_nav_msgs::msg::Waypoints waypoints_;
 };
 
 }  // namespace ike_nav
--- ike_waypoint_follower/src/ike_waypoint_follower.cpp
+++ ike_waypoint_follower/src/ike_waypoint_follower.cpp
@@ -3,12 +3,58 @@
 
 #include "ike_waypoint_follower/ike_waypoint_follower.hpp"
 
+#include <yaml-cpp/yaml.h>
+
 namespace ike_nav
 {
 
 IkeWaypointFollower::IkeWaypointFollower(const rclcpp::NodeOptions & options)
 : Node("ike_waypoint_follower", options)
 {
+  getParam();
+
+  readWaypointYaml();
+}
+
+void IkeWaypointFollower::getParam()
+{
+  this->param_listener_ =
+    std::make_shared<ike_waypoint_follower::ParamListener>(this->get_node_parameters_interface());
+  this->params_ = param_listener_->get_params();
+
+  waypoint_yaml_path_ = this->params_.waypoint_yaml_path;
+}
+
+void IkeWaypointFollower::readWaypointYaml()
+{
+  YAML::Node waypoints_yaml = YAML::LoadFile(waypoint_yaml_path_);
+
+  waypoints_.header.frame_id = "map";
+  waypoints_.header.stamp = rclcpp::Time();
+  waypoints_.waypoints.clear();
+
+  if (!waypoints_yaml["waypoints"].IsNull()) {
+    for (const auto & waypoint_yaml : waypoints_yaml["waypoints"]) {
+      ike_nav_msgs::msg::Waypoint waypoint;
+
+      waypoint.id = waypoint_yaml["id"].as<int32_t>();
+      waypoint.pose.position.x = waypoint_yaml["position"]["x"].as<double>();
+      waypoint.pose.position.y = waypoint_yaml["position"]["y"].as<double>();
+      waypoint.pose.orientation.w = cos(waypoint_yaml["euler_angle"]["z"].as<double>() / 2.);
+      waypoint.pose.orientation.z = sin(waypoint_yaml["euler_angle"]["z"].as<double>() / 2.);
+
+      for (const auto & function : waypoint_yaml["functions"]) {
+        if (function["function"].as<std::string>() == "variable_waypoint_radius") {
+          if (!function["waypoint_radius"].IsNull()) {
+            waypoint.function.variable_waypoint_radius.waypoint_radius =
+              function["waypoint_radius"].as<float>();
+          }
+        }
+      }
+
+      waypoints_.waypoints.push_back(waypoint);
+    }
+  }
 }
 
 }  // namespace ike_nav
