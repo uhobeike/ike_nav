67beb5a4fa51354811db80430596db2951958c70
Tatsuhiro Ikebe <beike315@icloud.com>
2023-09-06 20:49:35 +0900

    Feature Docker Demo tmux Auto (#46)
    * Add script
    
    * Remove C-m
    
    * Feature docker demo tmux auto
    
    * Fix URL

---
+++ .github/start_tmux.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+tmux new-session -d
+
+tmux send-keys "ros2 launch turtlebot3_gazebo turtlebot3_world.launch.py"
+
+tmux split-window -h
+
+tmux send-keys "ros2 launch ike_launch ike_nav.launch.xml"
+
+tmux attach-session
