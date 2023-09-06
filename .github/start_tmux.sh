33a11996af10b4775f52e30d7bdc81dbc7b3f652
uhobeike <beike315@icloud.com>
2023-09-06 21:21:25 +0900

    Feature tmux send keys clear

--- .github/start_tmux.sh
+++ .github/start_tmux.sh
@@ -2,10 +2,12 @@
 
 tmux new-session -d
 
+tmux send-keys "clear" C-m
 tmux send-keys "ros2 launch turtlebot3_gazebo turtlebot3_world.launch.py"
 
 tmux split-window -h
 
+tmux send-keys "clear" C-m
 tmux send-keys "ros2 launch ike_launch ike_nav.launch.xml"
 
 tmux attach-session
